7c920a43c58e9b4a45ffed6ba0136751c8d381d5
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-09-05 18:34:23 +0300

    init

---
+++ hw32.js
@@ -0,0 +1,53 @@
+`use strict`;
+
+(function () {
+    let calculator = {
+        read(a,b) {
+            this.a = +prompt(`enter arg a`);
+            this.b = +prompt(`enter arg b`);
+
+            if (isNaN(this.a) || isNaN(this.b)) {
+                errorText = `Enter number`;
+                throw errorText;
+            }
+        },
+
+        sum() {
+            return this.a + this.b;
+        },
+        mult() {
+            return this.a * this.b;
+        },
+    }
+
+    calculator.read();
+    console.log((calculator.sum()));
+    console.log((calculator.mult()));
+})();
+
+/*
+(function () {
+    let calculator2 = {
+        read2() {
+            a2 = +prompt(`enter arg a`);
+            b2 = +prompt(`enter arg b`);
+
+            if (isNaN(a2) || isNaN(b2)) {
+                errorText = `Enter number`;
+                throw errorText;
+            }
+        },
+
+        sum2() {
+            return a2 + b2;
+        },
+        mult2() {
+            return a2 * b2;
+        },
+    }
+
+    calculator2.read2();
+    console.log((calculator2.sum2()));
+    console.log((calculator2.mult2()));
+})();
+*/
